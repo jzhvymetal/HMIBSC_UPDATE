63793281fb27ba4ce12d64c17593042f685cd1fe
jzhvymetal <jzhvymetal@gmail.com>
2023-01-20 10:07:16 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 while getopts "s:n:" opt
 do
    case "$opt" in
-      n ) echo "Installing $OPTARG" | npm install -g --no-audit --no-update-notifier --no-fund --save --save-prefix=~ --production --engine-strict "$OPTARG" ;;
-      s ) echo "Installing $OPTARG" | npm install -g --strict-ssl false --registry https://ecostruxure-data-expert-essential.se.app:4873/ "$OPTARG" ;;
+      n ) echo "Installing" "$OPTARG" | npm install -g --no-audit --no-update-notifier --no-fund --save --save-prefix=~ --production --engine-strict "$OPTARG" ;;
+      s ) echo "Installing" "$OPTARG" | npm install -g --strict-ssl false --registry https://ecostruxure-data-expert-essential.se.app:4873/ "$OPTARG" ;;
    esac
 done

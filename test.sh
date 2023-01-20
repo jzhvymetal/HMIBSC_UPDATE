5fbf3aedfc419d80b374e15c44a0684c85a45bae
jzhvymetal <jzhvymetal@gmail.com>
2023-01-20 10:11:25 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 while getopts "s:n:" opt
 do
    case "$opt" in
-      n )   echo "Installing" "$OPTARG" 
+      n )   echo "Installing NodeJS Node:" "$OPTARG" 
             npm install -g --no-audit --no-update-notifier --no-fund --save --save-prefix=~ --production --engine-strict "$OPTARG" 
             ;;
-      s )   echo "Installing" "$OPTARG" 
+      s )   echo "Installing SE Node:" "$OPTARG" 
             npm install -g --strict-ssl false --registry https://ecostruxure-data-expert-essential.se.app:4873/ "$OPTARG" 
             ;;
    esac
